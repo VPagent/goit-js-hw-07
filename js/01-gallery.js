e9d073c9c89fddcfaca870098c2210b7301f95e7
VPagent <vpagent13@gmail.com>
2022-08-03 23:18:41 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery")
 
-console.log(galleryItems);
+let allPictures = ""
+
+galleryItems.forEach(obj  => {
+    allPictures += `<div class="gallery__item">
+    <a class="gallery__link" href="${obj.original}">
+      <img
+        class="gallery__image"
+        src="${obj.preview}"
+        data-source="${obj.original}"
+        alt="${obj.description}"
+      />
+    </a>
+  </div>`
+})
+
+galleryContainer.innerHTML= allPictures
+
+galleryContainer.addEventListener("click", onGalleryClick)
+
+function onGalleryClick(event){
+
+    event.preventDefault()
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" alt="${event.target.description}"/>`)
+    instance.show()
+}
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryContainer = document.querySelector(".gallery")
+
+let allPictures = ""
+
+galleryItems.forEach(obj  => {
+    allPictures += `<a class="gallery__item" href="${obj.original}">
+    <img class="gallery__image" src="${obj.preview}" alt="${obj.description}" />
+  </a>`
+})
+galleryContainer.innerHTML= allPictures
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
+
+console.log(lightbox)
+
 console.log(galleryItems);
